34af3190c0aedd132e4bb1f02d6a88225e0fcef4
Andy Orahoske <orahoske.2@gmail.com>
2016-12-12 13:30:50 -0500

    Add in timeline api route

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,4 +10,17 @@ class Api::UsersController < ApplicationController
 
     render json: { access_token: user.api_key, user_id: user.id }
   end
+
+  def timeline
+    user = User.find(params[:id])
+
+    followIds = user.followings.map { |follow| follow.id }
+    followIds.push(user.id) unless followIds.include? user.id
+    tweets = Tweet.where("user_id in (#{followIds.join(', ')})").order(created_at: :desc);
+
+    tweetResources = tweets.map { |tweetResource| Api::TweetResource.new(tweetResource, nil) }
+    serializedTweets = JSONAPI::ResourceSerializer.new(Api::TweetResource).serialize_to_hash(tweetResources)
+
+    render json: serializedTweets
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     post '/users/login', to: 'users#login'
+    get '/users/:id/timeline/', to: 'users#timeline'
     resources :users
     resources :tweets
     resources :follows
